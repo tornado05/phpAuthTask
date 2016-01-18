303fde7e7e36554914e2b992a090e98da9c0871d
hawk <h4wk13@gmail.com>
2016-01-18 22:08:59 +0200

    change to singletone

--- DAL/ConectorFactory.php
+++ DAL/ConectorFactory.php
@@ -7,20 +7,39 @@
         public function CreateConnector($storageType);
     }
 
-    class ConnectorFactory implements IConnectorFactory {
-        private $returnConnector = NULL;
+    class ConnectorFactory implements IConnectorFactory 
+    {
+        private static $_instance = NULL;
+   
+        private $_connector = NULL;
+    
+        private function __construct()
+        {
+        }
+        
+        public static function GetInstance($storageType)
+        {
+            if (self::$_instance == NULL)
+            {
+                self::$_instance = new DataLayerFactory();
+
+                switch($storageType)
+                {
+                    case FILE_STORAGE:
+                        $_connector = new FileTXT;
+                        break;
+                    case MYSQL_STORAGE:
+                        $_connector = new MySql;
+                        break;
+                }
+            }
+
+            return $_instance;
+        }
+    
         public function CreateConnector($storageType)
         {
-
-            switch(tolower($storageType)) {
-                case FILE_STORAGE:
-                    $returnConnector = new FileTXT;
-                    break;
-                case MYSQL_STORAGE:
-                    $returnConnector = new MySql;
-                    break;
-            }
-            return $returnConnector;
+            return $_connector;
         }
     }
 
