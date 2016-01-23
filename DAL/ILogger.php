6aca2c6f1fe287cd627aba670b9731c5523fa7db
Oleh Knyaz <thebeatle@mail.ru>
2016-01-23 17:28:23 +0200

    ILogger.php added

---
+++ DAL/ILogger.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 23.01.2016
+ * Tim
+ *
+ **/
+ interface ILogger
+ {
+    public function SetLogLevel($level);
+    public function Write($data);
+ }
