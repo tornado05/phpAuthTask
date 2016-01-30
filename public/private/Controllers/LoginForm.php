5fa4298cb04d5e01ad4fb19ce915c4e12f81196f
Petro Bazeliuk <pbazeliuk@gmail.com>
2016-01-30 14:11:34 +0200

    Added class Label

--- public/private/Controllers/LoginForm.php
+++ public/private/Controllers/LoginForm.php
@@ -1,14 +1,24 @@
 <?php
 
+require_once __DIR__ . '/Label.php';
+
 class LoginForm
 {
 	public function RenderHTML()
 	{
+		$labelLogin = new Label();
+		$labelLogin->value = 'LOGIN';
+		$labelPassword = new Label();
+		$labelPassword->value = 'PASSWORD';
+		
 		return 
-		'<form id="form_auth" method="post">
-			<label>LOGIN</label><br>
-			<input id="login" type="text" name="login"><br>
-			<label>PASSWORD</label><br>
+		'<form id="form_auth" method="post">'.
+			$labelLogin->RenderHTML().
+			'<br>
+			<input id="login" type="text" name="login">'.
+			'<br>'.
+			$labelPassword->RenderHTML().
+			'<br>
 			<input id="password" type="password" name="password"><br>
 			<input type="submit" value="LogIn">
 		</form>';
